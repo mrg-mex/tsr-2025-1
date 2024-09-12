d83deda776583cb8e384a08df2b3539e822ee036
Felipe Rivas <rivascf@gmail.com>
2024-09-12 16:45:43 -0600

    Modificado para pub/sub a un mensaje compuesto TargetPosition

--- demo_pkg/demo_pkg/MyNodo.py
+++ demo_pkg/demo_pkg/MyNodo.py
@@ -3,21 +3,39 @@
 import rclpy
 from rclpy.node import Node
 from std_msgs.msg import String
-from my_interface.msg import MyPosition
+from my_interface.msg import MyPosition, TargetPosition
 
 
 class MyNodo(Node):
     def __init__(self):
         super().__init__('my_node')
-        self._topic_pub = self.create_publisher(String, '/mensaje', 10)
+        self._topic_pub = self.create_publisher(MyPosition, '/mensaje', 10)
+        self.target_pos_pub = self.create_publisher(TargetPosition, '/target_pos', 10)
         self._timer = self.create_timer(3.0, self._timer_callback)
+        self._target_timer = self.create_timer(1.0, self._pub_trg_pos_clbk)
         self._count = 0
         self.get_logger().info("Nodo creado. ")
         
+    def _pub_trg_pos_clbk(self):
+        target_msg = TargetPosition()
+        # Header del mensaje
+        target_msg.header.frame_id = ''
+        target_msg.header.stamp = self.get_clock().now().to_msg()
+        # Etiqueta del punto destino
+        target_msg.destino = 'Punto destino'
+        # Variables de la posicion destino
+        target_msg.target_point.x = self._count / 5
+        target_msg.target_point.y = self._count / 2
+        target_msg.target_point.z = self._count / 3
+        self.target_pos_pub.publish(target_msg)
+
     def _timer_callback(self):
-        self._count += 1
-        mensaje = String()
-        mensaje.data = f"({self._count}) Repeticiones cada 3seg."
+        self._count += 1.0
+        mensaje = MyPosition()
+        mensaje.etiqueta = f"({self._count}) Repeticiones cada 3seg."
+        mensaje.pos_x = self._count
+        mensaje.pos_y = self._count / 3
+        mensaje.pos_z = self._count / 4
         self._topic_pub.publish(mensaje)
 
 def main(args=None):
--- demo_pkg/demo_pkg/MySubscriptor.py
+++ demo_pkg/demo_pkg/MySubscriptor.py
@@ -2,15 +2,20 @@
 
 import rclpy
 from rclpy.node import Node
-from std_msgs.msg import String
+from my_interface.msg import MyPosition, TargetPosition
 
 class MySecNodo(Node):
     def __init__(self, node_name):
         super().__init__(node_name=node_name)
-        self._topic_sub = self.create_subscription(String, '/mensaje', self._subs_callback, 10)
+        self._topic_sub = self.create_subscription(MyPosition, '/mensaje', self._subs_callback, 10)
+        self._target_pos_sub = self.create_subscription(TargetPosition, '/target_pos', self._tp_clbk, 10)
 
-    def _subs_callback(self, msg):
-        self.get_logger().info(f"Recibi ({msg.data})")
+    def _tp_clbk(self, tpmsg : TargetPosition):
+        self.get_logger().info(f"On [{tpmsg.header.stamp.sec, tpmsg.header.stamp.nanosec}]: To -> ({tpmsg.destino}), CurrPosXYZ ({tpmsg.target_point.x}, {tpmsg.target_point.y}, {tpmsg.target_point.z})")
+
+    def _subs_callback(self, msg : MyPosition):
+        #self.get_logger().info(f"Recibi ({msg.data})")
+        self.get_logger().info(f"Recibi [{msg.etiqueta}]: PosX = {msg.pos_x}, PosY = {msg.pos_y}, PosZ = {msg.pos_z},")
 
 def main(args=None):
     rclpy.init(args=args)
