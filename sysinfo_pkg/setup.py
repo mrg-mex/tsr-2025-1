238de78d476c411708191a1180432ced8af76037
Felipe Rivas <rivascf@gmail.com>
2024-11-12 16:27:02 -0600

    Agregue sysinfo_pkg

---
+++ sysinfo_pkg/setup.py
@@ -0,0 +1,25 @@
+from setuptools import find_packages, setup
+
+package_name = 'sysinfo_pkg'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='Felipe Rivas',
+    maintainer_email='rivascf@gmail.com',
+    description='Demo Servicios',
+    license='MIT',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
---
+++ sysinfo_pkg/sysinfo_pkg/arrg_utils/__init__.py
@@ -0,0 +1,11 @@
+"""
+arrg_utils package
+
+This package contains utilities for system information gathering and reporting.
+"""
+
+# Import SysInfo class for easy access
+from .sysinfo import SysInfo
+
+# Define public symbols for the package
+__all__ = ["SysInfo"]
